10f0d142911e6bc0ca2eb7fce678c579505dca59
Real man born on semtember <hieunam03@gmail.com>
2017-09-12 13:43:30 +0700

    Edit

--- jack_scripts.sh
+++ jack_scripts.sh
@@ -2,13 +2,11 @@
 
 
 #
-# FUCK JACK
-# Use with Android 7.X
+# JACK SCRIPTS
+# Use with Android 7.X and 8.X
 #
 
 # Kill the Jack server
-  printf "Fuck Jack...\n\n"
-  sleep 1
   ./prebuilts/sdk/tools/jack-admin kill-server
   rm -rf .jack-server
 
@@ -16,13 +14,10 @@
 #
 # Replace the value in the first of "ANDROID_JACK_VM_ARGS" with half of your RAM
 # (i.e.: if you have 8GB of ram, use "-Xmx4g")
-#
+# Set up jack scripts 
+
   export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4g"
-
-
-# You may need to uncomment the following command if Java tells you to use it.
-#  ulimit -n unlimited
-
+  ulimit -n unlimited
   ./prebuilts/sdk/tools/jack-admin install-server prebuilts/sdk/tools/jack-launcher.jar prebuilts/sdk/tools/jack-server-4.8.ALPHA.jar
   ./prebuilts/sdk/tools/jack-admin start-server
   gedit ~/.jack-server/config.properties 
